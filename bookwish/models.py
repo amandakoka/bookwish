d02ce4868317b845d87593e9f33002e8ec1c4ac9
amandakoka <amandakoka@icloud.com>
2024-04-09 17:24:26 +0100

    add: User and Book models

---
+++ bookwish/models.py
@@ -0,0 +1,16 @@
+from bookwish import db 
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_title = db.Column(db.String(255), nullable=False)
+    book_author = db.Column(db.String(255), nullable=False)
+    book_genre = db.Column(db.String(100), nullable=True)
+    book_link = db.Column(db.String(255), nullable=True)
+    has_read = db.Column(db.Boolean, default=False)
+
