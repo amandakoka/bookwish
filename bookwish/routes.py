5eb9c6fb2102fbc38f6c9f80bd103d3acb3043cb
amandakoka <amandakoka@icloud.com>
2024-04-12 22:22:04 +0100

    feat: added routes for displaying wishlist and adding books

--- bookwish/routes.py
+++ bookwish/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from bookwish import app, db
 from bookwish.models import User, Book
 
@@ -20,11 +20,23 @@ def login():
 
 @app.route("/wishlist")
 def wishlist():
-    return render_template("wishlist.html")
+    books = Book.query.all()
+    return render_template("wishlist.html", books=books)
 
 
 @app.route("/add_book", methods=["GET", "POST"])
 def add_book():
+    if request.method == "POST":
+        book = Book(
+            book_title=request.form.get("book_title"),
+            book_author=request.form.get("book_author"),
+            book_genre=request.form.get("book_genre"),
+            book_link=request.form.get("book_link"),
+            has_read=bool(True if request.form.get("has_read") else False),
+        )
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for("wishlist"))
     return render_template("add_book.html")
 
 
