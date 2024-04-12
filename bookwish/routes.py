e2456fba44139d61aa9923e84ec3c70d33820e68
amandakoka <amandakoka@icloud.com>
2024-04-13 00:20:59 +0100

    feat: add edit and delete book routes

--- bookwish/routes.py
+++ bookwish/routes.py
@@ -40,3 +40,28 @@ def add_book():
     return render_template("add_book.html")
 
 
+@app.route("/edit_book/<int:book_id>", methods=["GET", "POST"])
+def edit_book(book_id):
+    book = Book.query.get_or_404(book_id)
+
+    if request.method == "POST":
+        book.book_title = request.form.get("book_title")
+        book.book_author = request.form.get("book_author")
+        book.book_genre = request.form.get("book_genre")
+        book.book_link = request.form.get("book_link")
+        book.has_read = bool(request.form.get("has_read"))
+
+        db.session.commit()
+        return redirect(url_for('wishlist'))
+    return render_template("edit_book.html", book=book, book_id=book_id)
+
+
+@app.route("/delete_book/<int:book_id>", methods=["GET", "POST"])
+def delete_book(book_id):
+    book = Book.query.get_or_404(book_id)
+    if request.method == "POST":
+        db.session.delete(book)
+        db.session.commit()
+        return redirect(url_for('wishlist'))
+    return render_template("wishlist.html", book=book)
+
