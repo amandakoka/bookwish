d89253a9a04b42f53142dc6e23eb034e61698c3b
amandakoka <amandakoka@icloud.com>
2024-04-13 11:48:51 +0100

    docs: add comments to code

--- bookwish/__init__.py
+++ bookwish/__init__.py
@@ -1,13 +1,23 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-if os.path.exists("env.py"):
-    import env # noqa
 
+# Check if the "env.py" file exists and import environment variables if it does
+if os.path.exists("env.py"):
+    import env  # noqa
+
+# Create a Flask application instance
 app = Flask(__name__)
+
+# Configure the secret key for the application
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+# Configure the URI for the SQLAlchemy database
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+# Initialize a SQLAlchemy database instance with the Flask application
 db = SQLAlchemy(app)
 
-from bookwish import routes # noqa
+# Import routes from the "bookwish" package
+from bookwish import routes  # noqa
+
--- run.py
+++ run.py
@@ -1,12 +1,14 @@
-import os 
+import os
 from bookwish import app
 
-
+# Check if the script is being run directly
 if __name__ == "__main__":
+    # Run the Flask application
     app.run(
+        # Get the IP address from the environment variables
         host=os.environ.get("IP"),
+        # Get the port number from the environment variables and convert it to an integer
         port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG"),
-    )
-
-
+        # Get the debug mode status from the environment variables and convert it to a boolean
+        debug=os.environ.get("DEBUG")
+    ) 
