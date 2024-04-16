1b0e607a0ea595cc08c422ab7bf2705c5560055d
amandakoka <amandakoka@icloud.com>
2024-04-16 16:38:34 +0100

    update: string number for longer passwordhash

--- bookwish/models.py
+++ bookwish/models.py
@@ -4,7 +4,8 @@ from bookwish import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)
+
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
