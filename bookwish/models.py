e88fb3b0388cc2dfc2c214e29870eebeb53a643f
amandakoka <amandakoka@icloud.com>
2024-04-17 10:06:27 +0100

    feat: Add user_id field to Book model and define relationship with User model

--- bookwish/models.py
+++ bookwish/models.py
@@ -14,4 +14,7 @@ class Book(db.Model):
     book_genre = db.Column(db.String(100), nullable=True)
     book_link = db.Column(db.String(255), nullable=True)
     has_read = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref=db.backref('books',cascade='all, delete', lazy=True))
 
