77ec00b54932dac64c109371764e96d429d7922c
amandakoka <amandakoka@icloud.com>
2024-04-18 17:16:42 +0100

    Prepare Heroku files and configure database url

--- bookwish/__init__.py
+++ bookwish/__init__.py
@@ -8,14 +8,16 @@ if os.path.exists("env.py"):
 
 # Create a Flask application instance
 app = Flask(__name__)
-
-# Configure the secret key for the application
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-# Configure the URI for the SQLAlchemy database
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+     uri = os.environ.get("DATABASE_URL")
+     if uri.startswith("postgres://"):
+         uri = uri.replace("postgres://", "postgresql://", 1)
+     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
-# Initialize a SQLAlchemy database instance with the Flask application
 db = SQLAlchemy(app)
 
 # Import routes from the "bookwish" package
